3566a71196f813a06a9f0a45d427d4a327d59b8c
seharuem <seharuem@naver.com>
2026-02-10 22:22:21 +0900

    주민 명부 스타일 수정

--- src/pages/villager/villager.style.js
+++ src/pages/villager/villager.style.js
@@ -4,24 +4,24 @@ import { Glass, Scroll } from '../../components/style';
 const base = '/animal_guide';
 
 export const CardWrap = styled(Scroll).attrs({
-  className: 'w-full grid gap-2'
+	className: 'w-full grid gap-2 p-1'
 })`
-  grid-template-columns: repeat(auto-fill, minmax(7rem, 1fr));
-  grid-auto-rows: min-content;
+	grid-template-columns: repeat(auto-fill, minmax(7rem, 1fr));
+	grid-auto-rows: min-content;
 `;
 
 export const Mini = styled(Glass).attrs({
-  as: 'button',
-  type: 'button',
-  className: 'w-full pb-2 font-extrabold'
+	as: 'button',
+	type: 'button',
+	className: 'w-full pb-2 font-extrabold'
 })`
-  img {
-    width: 100%;
-  }
-  transition: background-color 0.2s ease-out;
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.4);
-  }
+	img {
+		width: 100%;
+	}
+	transition: background-color 0.2s ease-out;
+	&:hover {
+		background-color: rgba(255, 255, 255, 0.4);
+	}
 `;
 
 const fade = keyframes`
@@ -29,43 +29,46 @@ to { opacity: 1 }
 `;
 
 export const Card = styled.div.attrs({
-  className:
-    'flex flex-col h-90 p-10 max-sm:p-5 rounded-4xl flex-row gap-8 max-sm:gap-5 backdrop-blur-xl font-bold opacity-0 max-md:h-auto max-md:w-120 max-md:flex-col max-sm:w-100'
+	className:
+		'flex flex-col h-90 p-10 max-sm:p-5 rounded-4xl flex-row gap-8 max-sm:gap-5 backdrop-blur-xl font-bold opacity-0 max-md:h-auto max-md:w-120 max-md:flex-col max-sm:w-100'
 })`
-  border: 1px solid var(--c);
-  background-color: #776644;
-  color: rgba(255, 255, 255, 0.8);
-  box-shadow: 0 0 4px var(--c);
-  animation: ${fade} 0.2s ease-out forwards;
-  @media (max-width: 480px) {
-    width: 20rem;
-    padding: 1rem;
-  }
+	border: 1px solid var(--c);
+	background-color: #776644;
+	color: rgba(255, 255, 255, 0.8);
+	box-shadow: 0 0 4px var(--c);
+	animation: ${fade} 0.2s ease-out forwards;
+	@media (max-width: 480px) {
+		width: 20rem;
+		padding: 1rem;
+	}
 `;
 
 export const List = styled(Glass).attrs({
-  className:
-    'rounded-full py-2 px-4 text-base font-medium max-sm:text-sm flex gap-2 text-shadow-(--shadow)'
+	className:
+		'rounded-full py-2 px-4 text-base max-sm:text-sm flex gap-2 text-shadow-(--shadow) whitespace-nowrap'
 })`
-  &::before {
-    content: '';
-    width: 1rem;
-    aspect-ratio: 1;
-    background: url(${base}/link2.svg) center / contain no-repeat;
-  }
+	&::before {
+		content: '';
+		width: 1rem;
+		aspect-ratio: 1;
+		background: url(${base}/link2.svg) center / contain no-repeat;
+	}
+	@media (max-width: 480px) {
+		font-size: 0.75rem;
+    padding: 0.4rem 0.75rem;
+	}
 `;
 
 export const Close = styled(Glass).attrs({
-  as: 'button',
-  type: 'button',
-  className:
-    'rounded-full px-4 py-2 backdrop-blur-3xl text-sm font-bold opacity-0'
+	as: 'button',
+	type: 'button',
+	className: 'rounded-full px-4 py-2 backdrop-blur-3xl text-sm font-bold opacity-0'
 })`
-  box-shadow: 0 0 4px var(--c);
-  transition: background-color 0.1s ease-out;
-  background-color: rgba(255, 255, 255, 0.5);
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.8);
-  }
-  animation: ${fade} 0.2s ease-out forwards;
+	box-shadow: 0 0 4px var(--c);
+	transition: background-color 0.1s ease-out;
+	background-color: rgba(255, 255, 255, 0.5);
+	&:hover {
+		background-color: rgba(255, 255, 255, 0.8);
+	}
+	animation: ${fade} 0.2s ease-out forwards;
 `;
